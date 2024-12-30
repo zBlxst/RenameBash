1c089fa71925dddf3c6abd9dc3cb5255cf42207d
zblxst <varin.thomas.2002@gmail.com>
2024-12-30 14:33:03 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Auto rename files
+
+This is a little school project for an Advanced Scripts class.
+
+# How to use
+
+./rename.sh [OPTIONS]... "\<command\>" files
+
+Examples:
+
+// Reverses all the file names, asking for confirmation
+$ ./rename.sh -q -v "rev" *
+
+// Removes all file extensions
+$ ./rename.sh "rev | cut –d. -f2- | rev" *
+
+// Replace the initial of all files starting with "a." with an uppercase 
+$ ./rename.sh 'awk '\\''\{print toupper(substr(\$0, 1, 1)) substr(\$0, 2)\}'\\''' a.*
+
---
+++ rename.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+QFLAG=0
+VFLAG=0
+command=""
+args=""
+
+while test "$#" -ge 1; do
+    case $1 in 
+        -q) 
+            QFLAG=1
+            shift
+            continue;;
+        -v) 
+            VFLAG=1
+            shift
+            continue;;
+        -h)
+            echo "Usage"
+            exit;;
+        -*)
+            echo "Unkown option $1"
+            exit;;
+        *)
+            if test -z "$command"; then
+                command=$1
+            else
+                args="$args \"$1\""
+            fi
+            shift
+            continue;;
+    esac
+done
+
+eval "set to_shift $args"
+
+while test "$#" -ge 2; do
+    shift
+
+    if test ! -e "$1"; then 
+        echo "$1 doesn't exist"
+        continue
+    fi
+
+    A=$(eval "echo $1 | $command 2>/dev/null")
+    if test $? -ne 0; then
+        echo "Couldn't execute command    echo \"$1\" | $command   "
+        continue
+    fi
+
+
+    if test -e "$A"; then
+        echo
+        echo "\"$1\" -> \"$A\""
+        echo "\"$A\" already exists. Do you want to overwrite it ?"
+        read -p "Confirm (y/N)? : " CONFIRM
+        if test "$CONFIRM" != "y"; then
+            continue
+        fi
+    elif test $QFLAG -eq 1; then
+        echo
+        echo "\"$1 -> $A\""
+        read -p "Confirm (y/N)? : " CONFIRM
+        if test "$CONFIRM" != "y"; then
+            continue
+        fi
+    fi
+    
+    mv "$1" "$A"
+    if test $VFLAG -eq 1; then
+        echo "Renamed \"$1\" to \"$A\""
+    fi
+done
